7d523d85a663ec38a1659e332f11349c3fa604b7
Panu <qwsedrftgyhuiop1@gmail.com>
2024-10-19 23:38:18 +0700

    อัพเดตล่าสุด

--- fontend/src/app/admin-list/admin-list.component.ts
+++ fontend/src/app/admin-list/admin-list.component.ts
@@ -95,62 +95,12 @@ export class AdminListComponent implements OnInit {
     });
   }
 
-  // activities = [
-  //   {
-  //     name: "VDEG's 7th Volunteer Camp",
-  //     date: '16-22/03/23',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: false,
-  //   },
-  //   {
-  //     name: "VDEG's 9th Volunteer Camp",
-  //     date: '16-22/03/24',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: true,
-  //   },
-  //   {
-  //     name: "VDEG's 2nd Donation Fair",
-  //     date: '10/04/24',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: true,
-  //   },
-  //   {
-  //     name: "VDEG's 10th Volunteer Camp",
-  //     date: '16-22/10/24',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: false,
-  //   },
-  //   {
-  //     name: "VDEG's 1st Donation Fair",
-  //     date: '21/08/23',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: false,
-  //   },
-  //   {
-  //     name: "VDEG's 3rd Donation Fair",
-  //     date: '25/06/24',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: true,
-  //   },
-  //   {
-  //     name: "VDEG's 8th Volunteer Camp",
-  //     date: '16-22/10/23',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: true,
-  //   },
-  //   {
-  //     name: "VDEG's 6th Volunteer Camp",
-  //     date: '16-22/03/22',
-  //     informationLink: '#',
-  //     filesLink: '#',
-  //     status: false,
-  //   },
-  // ];
+  gotoReport(activityId: number): void {
+    this.router.navigate(['/activity-infor', activityId]);
+  }
+
+  gotoVolunteer(activityId: number): void {
+    this.router.navigate(['/activity-sinup-list', activityId]);
+  }
+
 }
